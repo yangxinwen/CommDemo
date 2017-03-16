ff46592cbf6d5437529ee8d2a0438f3d07623903
yang <yang@YANGXINWEN>
2017-03-16 18:27:51 +0800

    添加项目文件。

---
+++ XXJR.Communication/DuiTcpClient.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XXJR.Communication
+{
+    public class StatusChangeArgs
+    {
+        public DuiTcpClient Sender { get; set; }
+        public ConnectStatus Status { get; set; }
+    }
+
+
+    public class DuiTcpClient
+    {
+        private ConcurrentQueue<byte[]> _receivedDataQueue = new ConcurrentQueue<byte[]>();
+        public IPEndPoint EndPoint { get; set; }
+        public string IP
+        {
+            get
+            {
+                if (EndPoint != null)
+                    return EndPoint.Address.ToString();
+                else
+                    return string.Empty;
+            }
+        }
+        public int Port
+        {
+            get
+            {
+                if (EndPoint != null)
+                    return EndPoint.Port;
+                else
+                    return 0;
+            }
+        }
+        public TcpClient Client
+        {
+            get
+            {
+                return _client;
+            }
+        }
+        private TcpClient _client = null;
+        public DuiTcpClient()
+        {
+
+        }
+        public DuiTcpClient(TcpClient client)
+        {
+            if (client == null)
+                throw new Exception("client is null");
+            _client = client;
+            ReceiveData();
+            StartDistributeReceiveEvent();
+        }
+
+        public int MaxReciveBuffer { get; set; } = 1024;
+        private byte[] _receiveBuffer;
+
+        public event Action<byte[]> DataReceived;
+        public event Action<StatusChangeArgs> StatusChange;
+
+        public ConnectStatus ConnectStatus { get; } = ConnectStatus.Created;
+        public string SeesionId { get; } = Guid.NewGuid().ToString();
+
+        public void Connect()
+        {
+            if (EndPoint == null)
+                throw new Exception("EndPoint is null");
+            if (_client == null)
+            {
+                _client = new TcpClient();
+                _client.Connect(IP, Port);
+                if (_client.Connected == false)
+                {
+                    _client = null;
+                    return;
+                }
+
+                ReceiveData();
+                StartDistributeReceiveEvent();
+            }
+            _client.Connect(IP, Port);
+        }
+        private bool _isStartDistributing = false;
+        private void StartDistributeReceiveEvent()
+        {
+            if (_isStartDistributing)
+                return;
+            _isStartDistributing = true;
+
+            byte[] model = null;
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    if (_receivedDataQueue.IsEmpty)
+                    {
+                        Thread.Sleep(10);
+                        continue;
+                    }
+                    try
+                    {
+                        if (_receivedDataQueue.TryDequeue(out model))
+                        {
+                            DataReceived?.Invoke(model);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine(ex.Message);
+                    }
+                }
+            });
+        }
+
+        private bool _isReceiving = false;
+        private void ReceiveData()
+        {
+            if (_isReceiving)
+                return;
+            _isReceiving = true;
+
+            Task.Factory.StartNew(() =>
+            {
+                _receiveBuffer = new byte[MaxReciveBuffer];
+                int receiveCount = 0;
+                NetworkStream stream = null;
+                while (true)
+                {
+                    try
+                    {
+                        if (_client.Connected == false)
+                            continue;
+
+                        stream = _client.GetStream();
+                        receiveCount = stream.Read(_receiveBuffer, 0, _receiveBuffer.Length);
+                        stream = null;
+                        if (receiveCount <= 0)
+                        {
+                            Thread.Sleep(100);
+                            continue;
+                        }
+                        var bytes = new byte[receiveCount];
+                        Array.Copy(_receiveBuffer, 0, bytes, 0, receiveCount);
+                        _receivedDataQueue.Enqueue(bytes);
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine(ex.Message);
+                    }
+                }
+            });
+        }
+
+    }
+}
---
+++ XXJR.Communication/DuiTcpService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XXJR.Communication
+{
+    public class DuiTcpService
+    {
+        TcpListener _tcpListener = null;
+
+        public IPEndPoint EndPoint { get; set; }
+
+        public static Dictionary<string, DuiTcpClient> ClientList { get; } = new Dictionary<string, DuiTcpClient>();
+
+        public DuiTcpService()
+        {
+        }
+
+        public void Start()
+        {
+            if (EndPoint == null)
+                throw new Exception("Need Set EndPoint");
+            _tcpListener = new TcpListener(EndPoint);
+            _tcpListener.Start();
+            Task.Factory.StartNew(() => Listen());
+        }
+
+
+        public void Listen()
+        {
+            while (true)
+            {
+                _tcpListener.BeginAcceptTcpClient(new AsyncCallback(AcceptTcpClient), null);
+                Thread.Sleep(100);
+            }
+        }
+
+        public void AcceptTcpClient(IAsyncResult ar)
+        {
+            var remoteClient = new DuiTcpClient(_tcpListener.EndAcceptTcpClient(ar));
+            ClientList.Add(remoteClient.SeesionId, remoteClient);
+            remoteClient.DataReceived += RemoteClient_DataReceived;
+
+            remoteClient.StatusChange += (e) =>
+            {
+                if (e.Status == ConnectStatus.Fault)
+                    ClientList.Remove(e.Sender.SeesionId);
+            };
+        }
+
+
+        private void RemoteClient_DataReceived(byte[] obj)
+        {
+            Trace.WriteLine(obj.Length);
+        }
+    }
+}
